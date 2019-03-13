2085c129b51537b3a882eb7b416054618eaf28e1
Alexander Loboda <aleks.loboda@gmail.com>
2019-03-12 23:57:47 -0400

    windows.h and Rcpp.h conflict fix

--- src/include/mio.hpp
+++ src/include/mio.hpp
@@ -46,6 +46,11 @@
 #define MIO_PAGE_HEADER
 
 #ifdef _WIN32
+// Taken from http://tolstoy.newcastle.edu.au/R/e2/devel/06/11/1242.html
+// Undefine the Realloc macro, which is defined by both R and by Windows stuff
+#undef Realloc
+// Also need to undefine the Free macro
+#undef Free
 # include <windows.h>
 #else
 # include <unistd.h>
