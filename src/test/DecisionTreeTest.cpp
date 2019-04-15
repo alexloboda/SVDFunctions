b6b234c3cbba21787606b3dbfe7e544a874e8bcf
Alexander Loboda <aleks.loboda@gmail.com>
2019-04-15 12:56:55 -0400

    tests

--- src/test/DecisionTreeTest.cpp
+++ src/test/DecisionTreeTest.cpp
@@ -1,13 +1,21 @@
+#define CATCH_CONFIG_MAIN
 #include "../include/test/catch.hpp"
 #include "../include/genotype_predictor.h"
 
+namespace {
+    using namespace vcf;
+    const double EPS = 1e-8;
+}
+
 TEST_CASE("Decision trees constructed correctly"){
     SECTION("manual accuracy test") {
-        std::vector<std::vector<vcf::AlleleType>> data;
+        std::vector<std::vector<vcf::AlleleType>> data{{HOM, HET, HOM, HET, HOM, HET},
+                                                       {HOM, HOM, HOM, HET, HET, HET}};
 
-        std::vector<vcf::AlleleType> labels{};
-    }
-    SECTION("predicting test") {
-
+        std::vector<vcf::AlleleType> labels{HOMREF, HOMREF, HET, HET, HOM, HOM};
+        TreeBuilder tree_builder(data, labels, 2);
+        std::mt19937 random{42};
+        DecisionTree tree = tree_builder.build_a_tree(random);
+        REQUIRE(fabs(tree.accuracy() - 0.06746032) < EPS);
     }
 }
