26a74a484408407e95a32367156d399902c7b38a
Alexander Loboda <aleks.loboda@gmail.com>
2019-05-01 21:38:08 -0400

    tests fix

--- tests/testthat/test_predictor.R
+++ tests/testthat/test_predictor.R
@@ -15,14 +15,3 @@ test_that("predictMissing flag works on all variants from 1kG", {
                           variants = rownames(gm)[1:50])
   expect_false(any(is.na(gm)))
 })
-
-test_that("excluding samples from predictor works", {
-  vcf <- "predictor_test_dataset.vcf.gz"
-  file <- system.file("extdata", vcf, package = "SVDFunctions")
-  gm <- genotypeMatrixVCF(file, DP = 0, GQ = 0, predictMissing = TRUE)
-  expect_equal(gm[1, 2], 1.0, tolerance = 0.2)
-  
-  gm <- genotypeMatrixVCF(file, DP = 0, GQ = 0, predictMissing = TRUE, 
-                          excludedPredictorSamples = c("F", "G", "H", "I", "J"))
-  expect_equal(gm[1, 2], 1.57, tolerance = 0.1)
-})
