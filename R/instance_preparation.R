163dfff27cf4e3e1d3db9b34a6c979e5e632fecc
Mykyta Artomov <artomov@broadinstitute.org>
2019-05-03 14:59:39 -0400

    added test function for instance preparation with multiple clusters yaml wrap-up to be developed

---
+++ R/instance_preparation.R
@@ -0,0 +1,148 @@
+#' Estimate dendrogram for case clustering
+#' uses combiM output of clustCombi() function
+#' from Mclust package
+#' @param combiM combiM output of clustCombi() function
+#'
+dendrogramEstimate <- function(combiM){
+  stopifnot(class(combiM) == "list")
+  G <- length(combiM)
+  curr <- 1:G
+  merged <- -(1:G)
+  merge <- matrix(NA, G - 1, 2)
+  for (k in 1:(G - 1)) {
+    Kp = G - k + 1
+    l1 = which(!combiM[[Kp - 1]] %*% rep(1, Kp) == 1)
+    l2 = (combiM[[Kp - 1]] %*% curr)[l1] - curr[l1]
+    curr <- setdiff(curr, max(l1, l2))
+    merge[k, ] <- merged[c(l1, l2)]
+    merged[merged == merged[l1] | merged == merged[l2]] <- k
+  }
+  merge
+}
+
+
+#' Estimate clusters within case data
+#' Clusters are estimated from PC loadings using
+#' Gaussian mixed model fitting from Mclust package
+#' using ellipsoidal, varying volume, shape, and orientation
+#' model ("VVV")
+#' @param PCA results of gmatrix principal component analysis
+#' @param plot logical whether a plot of Bayesian Information 
+#' Criterion vs cluster number should be returned
+#' @export
+estimateCaseClusters <- function(PCA, plotBIC = FALSE, plotDendrogram = FALSE){
+  stopifnot(class(PCA) %in% c("matrix", "data.frame"))
+  
+  clResults <- mclust::Mclust(data = PCA, G = 1:20, modelNames = "VVV")
+  
+  if (plotBIC){
+    mclust::plot.Mclust(x = clResults, 
+                        what = "BIC", 
+                        xlab = "Number of Components", 
+                        ylab = "Bayesian Information Criterion")
+  }
+  
+  clCombi <- mclust::clustCombi(clResults)
+  collapsing <- dendrogramEstimate(clCombi)
+  
+  if (plotDendrogram){
+    mclust::combiTree(clCombi, type = "rectangle", yaxis = "entropy")
+  }
+  
+  list(clResults = clResults, collapsing = collapsing)
+}
+
+
+#' Estimate PCA from gmatrix without missing values and make 3dplot
+#' 
+#' PCA is estimated using SVD of the mean centered genotype matrix
+#' PCA coordinates will be needed to perform clustering of the 
+#' case samples to ensure relatively homogenious matching.
+#' @param gmatrix Genotype matrix wihout missing values
+#' @param clusters character vector of cluster assignment for every
+#' sample in gmatrix
+#' @export
+gmatrixPCA <- function(gmatrix, clusters = NULL){
+  pca <- RSpectra::svds(A = gmatrix - rowMeans(gmatrix), k = min(30, ncol(gmatrix)))$v
+  pca <- cbind(pca, clusters)
+  colnames(pca) <- c(paste("PC", c(1:min(30, ncol(gmatrix))), sep = ""),
+                     "Cluster")
+  p <- plotly::add_markers(plotly::plot_ly(x = PC1, 
+                                           y = PC2,
+                                           z = PC3, 
+                                           color = Cluster,
+                                           marker = list(size = 2 )))
+  
+  list(PCA = pca, plot = p)  
+}
+
+
+#' QC gmatrix
+#' performs callrate estimate on samples and variants
+#' checks for Hardy-Weinberg equilibrium and finds consistent
+#' set of variants across all clusters
+#' 
+
+gmatrixQC <- function(gmatrix, imputationResults, clusters, minVariantCallRate = 0.95,
+                      minSampleCallRate = 0.95){
+  sampleCallRate <- apply(imputationResults, MARGIN = 2, function(x){
+    length(x[x == 0]) / length(x) })
+  gmatrix <- gmatrix[, which(sampleCallRate > minSampleCallRate)]
+  
+  variantCallRate <- apply(imputationResults, MARGIN = 1, function(x){
+    length(x[x == 0]) / length(x) })
+  gmatrix <- gmatrix[which(variantCallRate > minVariantCallRate), ]
+  
+  pass_variants <- vector("list", length(unique(clusters)))
+  for (i in 1:length(unique(clusters))){
+    pass_variants <- genotypesToCounts()
+  }
+  
+}
+
+
+
+
+prepareInstance1 <- function(gmatrix, imputationResults, minVariantCallRate = 0.95,
+                             minSampleCallRate = 0.95){
+  sampleCallRate <- apply(imputationResults, MARGIN = 2, function(x){
+    length(x[x == 0]) / length(x) })
+  gmatrix <- gmatrix[, which(sampleCallRate > minSampleCallRate)]
+  
+  variantCallRate <- apply(imputationResults, MARGIN = 1, function(x){
+    length(x[x == 0]) / length(x) })
+  gmatrix <- gmatrix[which(variantCallRate > minVariantCallRate), ]
+  
+  pca <- RSpectra::svds(A = gmatrix - rowMeans(gmatrix), 
+                        k = min(30, ncol(gmatrix)))$v
+  
+  clResults <- mclust::Mclust(data = pca, G = 1:20, modelNames = "VVV")
+  clCombi <- mclust::clustCombi(clResults)
+  collapsing <- dendrogramEstimate(clCombi)
+  
+  numberOfClusters <- length(unique(clResults$classification))
+  
+  case_counts <- vector("list", numberOfClusters)
+  for (i in 1:numberOfClusters){
+    case_counts[[i]] <- 
+      genotypesToCounts(gmatrix[, which(clResults$classification == i)])
+  }
+  pass_variants <- lapply(case_counts, checkAlleleCounts)
+  pass_variants <- lapply(pass_variants, function(x){
+    which(x)
+  })
+  pass_variants <- Reduce(intersect, pass_variants)
+  gmatrix <- gmatrix[pass_variants, ]
+  clusterResults <- vector("list", numberOfClusters)
+  for (i in 1:numberOfClusters){
+    
+    svdResult <- 
+      RSpectra::svds(A = gmatrix[, which(clResults$classification == i)], 
+                     k = length(clResults[clResults$classification == i]))
+    US <- svdResult$u %*% diag(svdResult$d)
+    counts <- genotypesToCounts(gmatrix[, which(clResults$classification == i)])
+    clusterResults[[i]] <- list(US = US, counts = counts)
+  }
+  names(clusterResults) <- c(1:numberOfClusters)
+  list(population = clusterResults, variants = rownames(gmatrix))
+}
