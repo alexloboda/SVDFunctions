276216fc54deceaf99989f4839a49d8008d682b8
Alexander Loboda <aleks.loboda@gmail.com>
2025-06-25 09:30:06 +0300

    prepareInstance to save samples left for matching

--- R/prepareInstance.R
+++ R/prepareInstance.R
@@ -109,24 +109,24 @@ gmatrixPCA <- function(gmatrix, SVDReference = NULL, referenceMean = NULL, compo
   if (is.null(referenceMean)) {
     referenceMean <- rowMeans(gmatrix)
   }
-  
+
   if (is.null(SVDReference)) {
     SVDReference <- RSpectra::svds(gmatrix - referenceMean, k = components)$u
     rownames(SVDReference) <- rownames(gmatrix)
   }
-  
+
   variants <- intersect(rownames(SVDReference), rownames(gmatrix))
   gmatrix <- gmatrix[variants, ]
-  
+
   refVariants <- stats::setNames(1:nrow(SVDReference), rownames(SVDReference))
   ids <- refVariants[variants]
-  
+
   SVDReference <- SVDReference[ids, ]
   referenceMean <- referenceMean[ids]
-  
+
   gmatrix <- gmatrix - referenceMean
   pca <- t(SVDReference) %*% gmatrix
-  
+
   pca <- pca[1:components, ]
   pca <- t(pca)
   colnames(pca) <- c(paste("PC", 1:components, sep = ""))
@@ -148,18 +148,18 @@ filterGmatrix <- function(gmatrix, imputationResults, minVariantCallRate = 0.95,
     sum(!x) / length(x)
   })
   gmatrix <- gmatrix[, which(sampleCallRate > minSampleCallRate)]
-  
+
   variantCallRate <- apply(imputationResults, 1, function(x) {
     sum(!x) / length(x) 
   })
-  
+
   gmatrix <- gmatrix[which(variantCallRate > minVariantCallRate), ]
   imputationResults <- imputationResults[variantCallRate > minVariantCallRate, 
                                          sampleCallRate > minSampleCallRate]
-  
+
   message("Kept ", ncol(gmatrix), " out of ", nSamples, " individuals")
   message("Kept ", nrow(gmatrix), " out of ", nVariants, " variants")
-  list(gmatrix = gmatrix, imputationResults = imputationResults)
+  list(genotype = gmatrix, predicted = imputationResults)
 }
 
 #' Plot 3d PCA plot.
@@ -265,11 +265,11 @@ drop <- function(pca, knn_rate, mvn_rate) {
 #' Prepare instance and write yaml file from QC-ed gmatrix.
 #' 
 #' @param gmatrix gmatrix with imputed missing values.
-#' @param imputationResults matrix of the same dimensions as \code{gmatrix} 
+#' @param imputationResults matrix of the same dimensions as \code{gmatrix}
 #' filled with values TRUE/FALSE indicating which genotypes were imputed.
 #' @param controlsU Reference U matrix from SVD decomposition of centered
 #' genotype matrix of control dataset or joint dataset.
-#' @param meanControl numeric vector representing mean per-variant genotype, 
+#' @param meanControl numeric vector representing mean per-variant genotype,
 #' that were taken as a center while costructing \code{controlsU} matrix.
 #' @param outputFileName name of the YAML file to output.
 #' @param clusters clustering object.
@@ -277,16 +277,22 @@ drop <- function(pca, knn_rate, mvn_rate) {
 #' @param MAC minor allele count used for QC.
 #' @param MAF minor allele frequency used for QC.
 #' @param knn_drop fraction of samples that would be excluded from dataset at
-#' each level of hierarchy clustering by applying knn anomaly detection algorithm.
+#' each level of hierarchy clustering by applying knn anomaly
+#' detection algorithm.
 #' @param normalize_drop fraction of samples to be excluded from dataset at
-#' each level of hierarchy clustering by applying function \code{normal_subsample} 
+#' each level of hierarchy clustering by applying
+#' function \code{normal_subsample}
 #' from the package.
+#' @param keptSamplesFile Optional. File to save the names of kept samples.
 #' @import mclust
 #' @export
-prepareInstance <- function (gmatrix, imputationResults, controlsU, meanControl, 
-                             outputFileName, clusters = NULL, 
-                             title = "DNAScoreInput", MAC = 10, MAF = 0.01, 
-                             knn_drop = 0.05, normalize_drop = 0.05) {
+prepareInstance <- function(
+  gmatrix, imputationResults, controlsU, meanControl,
+  outputFileName, clusters = NULL, 
+  title = "DNAScoreInput", MAC = 10, MAF = 0.01, 
+  knn_drop = 0.05, normalize_drop = 0.05,
+  keptSamplesFile = paste0(format(Sys.time(), "%Y%m%d_%H%M%S"), ".samples.RDS")
+) {
   if (is.null(clusters)) {
     classes <- rep("Main", ncol(gmatrix))
     clusters <- clustering(setNames(classes, colnames(gmatrix)), 
@@ -329,17 +335,17 @@ prepareInstance <- function (gmatrix, imputationResults, controlsU, meanControl,
   } else {
     gm_names <- colnames(gmatrix)
   }
-  
+
   clusters$hier$Do(function(node) {
     i <- ifelse(is.null(node$id), return(), node$id)
     cluster_leaves <- sapply(node$leaves, function(x) x$id)
-    
+
     initial_cluster <- which(clusters$samples %in% cluster_leaves)
     clusterGenotypes <- controlsU %*% (gmatrix[, initial_cluster] - meanControl)
-    
+
     cluster <- initial_cluster[drop(clusterGenotypes, knn_drop, normalize_drop)]
     clusterGenotypes <- controlsU %*% (gmatrix[, cluster] - meanControl)
-    
+
     clusterGenotypesForCounts <- gmatrixForCounts[, cluster]
     clusterMeans <- rowMeans(clusterGenotypes)
     k <- min(nrow(clusterGenotypes), ncol(clusterGenotypes))
@@ -347,15 +353,19 @@ prepareInstance <- function (gmatrix, imputationResults, controlsU, meanControl,
     svdResult <- suppressWarnings(RSpectra::svds(A = clusterGenotypes, k = k))
     US <- svdResult$u %*% diag(svdResult$d)
     US <- US / sqrt(length(cluster))
-    
+
     counts <- genotypesToCounts(clusterGenotypesForCounts)
     cluster_ids[[i]] <<- gm_names[cluster]
     clusterResults[[i]] <<- list(US = US, counts = counts, 
                                 mean = clusterMeans, 
                                 title = clusters$classes[i])
   })
-  writeYaml(clusterResults, clusters, variants = rownames(gmatrix), 
+  salt <- writeYaml(clusterResults, clusters, variants = rownames(gmatrix), 
             outputFileName = outputFileName, title = title)
+
+  if (!is.null(keptSamplesFile)) {
+    saveRDS(cluster_ids, keptSamplesFile)
+  }
   cluster_ids
 }
 
@@ -377,7 +387,8 @@ getNamesFromHier <- function(hier) {
     if (length(hier$split) != 3) {
       userError("Incorrect hierarchy section in the input YML file.")
     }
-    ret <- c(getNamesFromHier(hier$split$left), getNamesFromHier(hier$split$right))
+    ret <- c(getNamesFromHier(hier$split$left), 
+             getNamesFromHier(hier$split$right))
     name <- paste(ret, collapse = " & ")
     c(ret, setNames(name, hier$split$id))
   }  else {
@@ -385,7 +396,7 @@ getNamesFromHier <- function(hier) {
       userError("Incorrect hierarchy section in the input YML file.")
     }
     stats::setNames(hier$cluster$name, hier$cluster$id)
-  } 
+  }
 }
 
 readMatrix <- function(obj, nvars, name) {
